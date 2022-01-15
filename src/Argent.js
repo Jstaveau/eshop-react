9de1221c0fbfc2da9715fa4872538cf4753d8995
jonathan <jonathan.staveau@outlook.com>
2022-01-15 16:24:06 +0100

    stock css  ok

--- src/Argent.js
+++ src/Argent.js
@@ -16,27 +16,49 @@ export default function Argent() {
 
     const [argent, setArgent] = useState(20)
 
+    // const [stockFaible, setStockFaible] = useState(false)
+
+    // const verifStock = (article) => {
+    //     if (article.stock > 0 && article.stock <2) {
+    //         setStockFaible(!stockFaible)
+    //     }
+    // }
+
+    const acheter = (article) => {
+        article.stock -= 1
+        setArgent(argent - article.prix)
+    }
+
     return (
         <section>
             <h1 className='titre-argent'>Mon argent : {argent}€</h1>
             <div className='produits'>
-                <Article 
+                <Article
+                class={ articles[0].stock < 2 && articles[0].stock > 0 ? 'description stock-faible' : 'description' }
+                classRed={ articles[0].stock < 1 ? ' stock-null' : '' }
                 nom={articles[0].nom}
                 prix={articles[0].prix}
                 stock={articles[0].stock}
                 img={articles[0].img}
+                acheter={() => acheter(articles[0])}
                 />
                 <Article 
+                class={ articles[2].stock < 2 && articles[2].stock > 0 ? 'description stock-faible' : 'description' }
+                classRed={ articles[2].stock < 1 ? ' stock-null' : '' }
                 nom={articles[2].nom}
                 prix={articles[2].prix}
                 stock={articles[2].stock}
                 img={articles[2].img}
+                acheter={() => acheter(articles[2])}
                 />
                 <Article 
+                class={ articles[1].stock < 2 && articles[1].stock > 0 ? 'description stock-faible' : 'description' }
+                classRed={ articles[1].stock < 1 ? ' stock-null' : '' }
                 nom={articles[1].nom}
                 prix={articles[1].prix}
                 stock={articles[1].stock}
                 img={articles[1].img}
+                acheter={() => acheter(articles[1])}
                 />
             </div>
             <Panier />
--- src/Article.js
+++ src/Article.js
@@ -4,11 +4,14 @@ export default function Article(props) {
     return (
         <div className='produit'>
             <img src={props.img} alt="img article" />
-            <div className='description'>
+            <div className={props.class + props.classRed}>
                 <h2>{props.nom}</h2>
                 <p>Prix : {props.prix}€</p>
                 <p>Stock : {props.stock} unités.</p>
-                <button>ACHETER</button>
+                <button onClick={ () => {
+                    props.acheter()
+                    props.verif()
+                    }}>ACHETER</button>
             </div>
         </div>
     )
